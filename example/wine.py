b0328deaa2cf13c2849d915ea17ebbd4cf00007a
Aiga SUZUKI <ai-suzuki@aist.go.jp>
2017-05-30 09:50:36 +0900

    initial commit.
    Onesan could work eternally

---
+++ example/wine.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('../onesan')
+import onesan
+import pandas
+import numpy as np
+
+
+data_df = pandas.read_csv('./winequality-white.csv')
+data = data_df.as_matrix()
+
+X = data[:, [0,1,2,3,4,5,6,7,8,9,10]]
+Y = data[:, 11]
+
+obj = onesan.Onesan(X, Y)
+obj.run()
---
+++ onesan/onesan.py
@@ -0,0 +1,57 @@
+import sklearn.svm as SVM
+from sklearn.model_selection import train_test_split
+import copy
+from tqdm import tqdm
+
+
+class Onesan(object):
+
+    def __init__(self, X, Y, train_size=0.8, classifier=None, classifier_param=None):
+
+        # divide dataset into train and test
+        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(
+            X, Y, test_size=1 - train_size
+        )
+
+        # number of dimensions
+        self.Xdim = self.X_train.shape[-1]
+        self.combinations = 2 ** self.Xdim
+
+        # set classifier
+        if classifier is not None:
+            if not hasattr(classifier, 'fit') or not hasattr(classifier, 'predict'):
+                raise TypeError('classifier must have "fit" and "predict" method.')
+
+            self.classifier = classifier
+
+        else:
+            if classifier_param is None:
+                self.classifier = SVM.LinearSVC(C=0.05, random_state=1, max_iter=5000)
+            else:
+                self.classifier = SVM.LinearSVC(**classifier_param)
+
+    def to_selectvec(self, code):
+        bcode = format(code, '011b')
+        return list(map(int, list(bcode)))
+
+    def pick_andvalue(self, X, code):
+        slist = list()
+        for i, b in enumerate(code):
+            if b == 1:
+                slist.append(i)
+
+        return X[:, slist]
+
+    def run(self):
+
+        result = list()
+
+        for i in tqdm(range(1, self.combinations)):
+            classifier = copy.deepcopy(self.classifier)
+            # train classifier
+            classifier.fit(self.pick_andvalue(self.X_train, self.to_selectvec(i)), self.Y_train)
+
+            pred = classifier.predict(self.pick_andvalue(self.X_test, self.to_selectvec(i)))
+            mse = ((pred - self.Y_test) ** 2).mean(axis=None)
+
+            result.append([1, ''.join(map(str, self.to_selectvec(i))), mse])
