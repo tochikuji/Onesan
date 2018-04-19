83aa2bdb1e37dbdb487b9aa742c75e07e7b0f5ea
Aiga SUZUKI <ai-suzuki@aist.go.jp>
2018-04-19 15:56:47 +0900

    update setup.py; version 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='Onesan',
-      version='0.1.2',
+      version='0.1.3',
       description='Bruteforce feature selector',
       author='Aiga SUZUKI',
       author_email='ai-suzuki@aist.go.jp',
